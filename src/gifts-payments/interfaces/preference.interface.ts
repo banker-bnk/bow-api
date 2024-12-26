de78c4f3a8f0766a966abaf7e6941a9155fa8379
Christian Morán <cmoran4@llbean.com>
2024-12-26 17:00:32 -0300

    FEAT: MON-28: Delete unnecessary interfaces

--- src/gifts-payments/interfaces/preference.interface.ts
+++ src/gifts-payments/interfaces/preference.interface.ts
@@ -5,38 +5,3 @@ export interface IPreferenceBody {
   userId: string;
   message?: string;
 }
-
-export interface IUser {
-  userId: string;
-  userName?: string;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  phone?: string;
-  address?: string;
-  image?: string;
-  birthday?: Date;
-  lastSeen?: Date;
-}
-
-export interface IGift {
-  id: number;
-  title?: string;
-  description?: string;
-  link?: string;
-  price?: number;
-  currency?: string;
-  endDate?: Date;
-  image?: string;
-  user?: IUser;
-  createdAt?: Date;
-}
-
-export interface IPaymentInfo {
-  gift: IGift;
-  user: IUser;
-  amount: number;
-  currency: string;
-  source: string;
-  createdAt: Date;
-}
