93d224640b200aad916a873259054b84bcf4de49
Christian Morán <cmoran4@llbean.com>
2025-01-07 23:35:36 -0300

    FEAT: MON-17: Refine gifts endpoints

--- src/gifts/entities/gift.ts
+++ src/gifts/entities/gift.ts
@@ -4,38 +4,53 @@ import {
   Column,
   CreateDateColumn,
   ManyToOne,
+  OneToMany,
 } from 'typeorm';
 import { User } from '../../users/entities/user';
+import { ApiProperty } from '@nestjs/swagger';
+import { GiftsPayment } from '../../gifts-payments/entities/gifts-payment';
 
 @Entity('gifts')
 export class Gift {
   @PrimaryGeneratedColumn()
+  @ApiProperty({ example: 1 })
   id: number;
 
   @Column()
+  @ApiProperty({ example: 'Birthday gift' })
   title: string;
 
   @Column()
+  @ApiProperty({ example: 'Description' })
   description: string;
 
   @Column()
+  @ApiProperty({ example: 'www.bow.com' })
   link: string;
 
   @Column('decimal')
+  @ApiProperty({ example: 12.99 })
   price: number;
 
   @Column()
+  @ApiProperty({ example: 'ARS' })
   currency: string;
 
   @Column({ nullable: true })
+  @ApiProperty({ example: '2025-01-07T21:42:46.121Z' })
   endDate: Date;
 
   @Column({ nullable: true })
+  @ApiProperty({ example: 'Image URL' })
   image: string;
 
   @ManyToOne(() => User, (user) => user.gifts, { onDelete: 'RESTRICT' })
   user: User;
 
   @CreateDateColumn()
+  @ApiProperty({ example: '2025-01-07T21:42:46.121Z' })
   createdAt: Date;
+
+  @OneToMany(() => GiftsPayment, (giftsPayment) => giftsPayment.gift)
+  giftsPayments: GiftsPayment[];
 }
--- src/gifts/gifts.service.ts
+++ src/gifts/gifts.service.ts
@@ -10,13 +10,18 @@ export class GiftsService {
     private giftsRepository: Repository<Gift>,
   ) {}
 
-  findAll() {
+  findAll(): Promise<Gift[]> {
     return this.giftsRepository.find();
   }
 
-  create(data: Partial<Gift>) {
+  create(data: Partial<Gift>): Promise<Gift> {
     const gift = this.giftsRepository.create(data);
     return this.giftsRepository.save(gift);
+  }
+
+  async update(id: number, data: Partial<Gift>): Promise<Gift> {
+    await this.giftsRepository.update(id, data);
+    return this.giftsRepository.findOneBy({ id });
   }
 
   async delete(data: { id:number; user: number }) {
@@ -34,5 +39,33 @@ export class GiftsService {
     }
 
     await this.giftsRepository.remove(gift);
+  }
+
+  async findById(id: number) {
+    const gift = await this.giftsRepository
+      .createQueryBuilder('gift')
+      .leftJoinAndSelect('gift.giftsPayments', 'giftsPayments')
+      .leftJoinAndSelect('giftsPayments.user', 'user')
+      .select([
+        'gift',
+        'giftsPayments.amount',
+        'user.id',
+        'user.userName',
+        'user.lastName',
+        'user.image',
+      ])
+      .where('gift.id = :id', { id })
+      .getOne();
+
+    const totalAmount = gift.giftsPayments
+      .reduce((sum, payment) => {
+        return sum + Number(payment.amount);
+      }, 0)
+      .toFixed(2);
+
+    return {
+      ...gift,
+      totalAmount,
+    };
   }
 }
