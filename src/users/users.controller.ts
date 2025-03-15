967b28bf11247bbc8d0bb239bed3482f09e3ac75
matias <team@autoapps.ai>
2025-03-15 19:52:54 -0300

    -

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -185,6 +185,7 @@ export class UsersController {
 
   @Get('search-non-friends')
   @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth('JWT')
   @ApiOperation({
     summary: 'Search users by search term (userName, firstName, lastName, or email) not including current friends',
     description: 'Search users by search term, matching userName, firstName, lastName, or email, not including current friends.',
