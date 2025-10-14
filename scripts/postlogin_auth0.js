122bf01641aed4fb7f6b81219c1e8669db606327
matias <team@autoapps.ai>
2025-10-14 09:32:32 -0300

    -

--- scripts/postlogin_auth0.js
+++ scripts/postlogin_auth0.js
@@ -6,52 +6,57 @@
  * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
  */
 exports.onExecutePostLogin = async (event, api) => {
-    const userId = event.user.user_id;
-    const userName = event.user.username ?? event.user.email ?? null;
-    const email = event.user.email ?? null;
-    const firstName = event.user.given_name ?? null;
-    const lastName = event.user.family_name ?? null;
-    const phone = event.user.phone_number ?? event.user.user_metadata?.phone ?? null;
-    const address = event.user.user_metadata?.address ?? null;
-    const image = event.user.picture ?? null;
-    const birthday = event.user.user_metadata?.birthday ?? null;
-  
-    console.log("Newly Registered User:", JSON.stringify({
-      userId,
-      userName,
-      email,
-      firstName,
-      lastName,
-      phone,
-      address,
-      image,
-      birthday
-    }));
-  
-    const url = "https://mpm.ddns.net/users";
-  
-    try {
-      const res = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify({
-          userId,
-          userName,
-          email,
-          firstName,
-          lastName,
-          phone,
-          address,
-          image,
-          birthday
-        })
-      });
-  
-      const data = await res.json();
-      console.log("API response:", data);
-    } catch (err) {
-      console.error("Error calling user creation API:", err);
-    }
-  };
+  const isFirstLogin = event.stats?.logins_count === 1;
+  if (!isFirstLogin) {
+      console.log("PostLogin: not first login, skipping user POST");
+      return;
+  }
+  const userId = event.user.user_id;
+  const userName = event.user.username ?? event.user.email ?? null;
+  const email = event.user.email ?? null;
+  const firstName = event.user.given_name ?? null;
+  const lastName = event.user.family_name ?? null;
+  const phone = event.user.phone_number ?? event.user.user_metadata?.phone ?? null;
+  const address = event.user.user_metadata?.address ?? null;
+  const image = event.user.picture ?? null;
+  const birthday = event.user.user_metadata?.birthday ?? null;
+
+  console.log("Newly Registered User:", JSON.stringify({
+    userId,
+    userName,
+    email,
+    firstName,
+    lastName,
+    phone,
+    address,
+    image,
+    birthday
+  }));
+
+  const url = "https://mpm.ddns.net/users";
+
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        userId,
+        userName,
+        email,
+        firstName,
+        lastName,
+        phone,
+        address,
+        image,
+        birthday
+      })
+    });
+
+    const data = await res.json();
+    console.log("API response:", data);
+  } catch (err) {
+    console.error("Error calling user creation API:", err);
+  }
+};
