ec3905171786c7f7d0f23bcd1e8968832733a7fc
matias <team@autoapps.ai>
2025-11-01 17:09:33 -0300

    -

--- src/friend-invitations/friend-invitations.service.ts
+++ src/friend-invitations/friend-invitations.service.ts
@@ -74,11 +74,11 @@ export class FriendInvitationsService {
     console.log("111 sender stringify: " + JSON.stringify(sender));
     console.log("111 sender.id: " + sender.id);
     console.log("111 sender " + sender);
-    console.log("111 sender parse int " + parseInt(sender.id.toString()));
+    //console.log("111 sender parse int " + parseInt(sender.id.toString()));
     console.log("111 sender parse int to number " + Number(sender.id));
     console.log("111 sender stringify: " + parseInt(JSON.stringify(sender)));
     const senderUser = await this.usersService.findById(parseInt(JSON.stringify(sender)));
-    
+
     console.log("222 senderUser: " + JSON.stringify(senderUser));
 
 
